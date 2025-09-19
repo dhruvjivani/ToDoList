714954733527e961b38b1ff50f3221d90e210b19
dhruvjivanI <dhruvjivani001@gmail.com>
2025-09-18 20:59:00 -0400

    Initial Commit

---
+++ 9044406_Assignment_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  9044406_Assignment_2
+//
+//  Created by Dhruv Rasikbhai Jivani on 9/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 9044406_Assignment_2/_044406_Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  _044406_Assignment_2App.swift
+//  9044406_Assignment_2
+//
+//  Created by Dhruv Rasikbhai Jivani on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct _044406_Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
